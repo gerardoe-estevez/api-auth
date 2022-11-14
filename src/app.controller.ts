c19fd377b04f0c362d7e4f1ebc41d9e1090e3ff3
gerardoe-estevez <gerardoestevez79@gmail.com>
2022-11-14 04:50:46 -0400

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,97 @@
+import {
+  Controller,
+  Post,
+  Body,
+  BadRequestException,
+  Res,
+  Get,
+  Req,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt/dist';
+import { Response, Request } from 'express';
+
+@Controller('api')
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private jwtService: JwtService,
+  ) {}
+
+  @Post('register')
+  async register(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const saltOrRounds = 12;
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+
+    const user = await this.appService.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    delete user.password;
+
+    return user;
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const user = await this.appService.findOne({ where: { email } });
+
+    if (!user) {
+      throw new BadRequestException('invalid user');
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new BadRequestException('invalid credentials');
+    }
+
+    const jwt = await this.jwtService.signAsync({ id: user.id });
+
+    response.cookie('jwt', jwt, { httpOnly: true });
+
+    return {
+      message: 'success',
+    };
+  }
+
+  @Get('user')
+  async user(@Req() request: Request) {
+    try {
+      const cookie = request.cookies['jwt'];
+
+      const data = await this.jwtService.verifyAsync(cookie);
+
+      if (!data) {
+        throw new UnauthorizedException();
+      }
+
+      const id = data['id'];
+      const user = await this.appService.findOne({ where: { id } });
+
+      const { password, ...result } = user;
+
+      return result;
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+  }
+
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) response: Response) {
+    response.clearCookie('jwt');
+
+    return {
+      message: 'success',
+    };
+  }
+}
---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'containers-us-west-130.railway.app',
+      port: 6551,
+      username: 'root',
+      password: 'BWz64V7Cml3smaxCpZIR',
+      database: 'railway',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
